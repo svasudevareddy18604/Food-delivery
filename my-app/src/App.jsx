6dc938fdce6592ac9375b16eab5181eb1dd90597
sunkireddy vasudeva reddy <svasudevareddy18604@gmail.com>
2026-05-23 20:20:01 +0530

    ADDED DASHBAORD AND UPDATED SINGUP PAGE

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,45 +1,7 @@
-import './App.css'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-  return (
-    <div className="container">
-
-      <nav className="navbar">
-        <h2>Foodie</h2>
-
-        <button>Login</button>
-      </nav>
-
-      <section className="hero">
-
-        <div className="left">
-
-          <h1>
-            Fast Food <br />
-            Delivery
-          </h1>
-
-          <p>
-            Order tasty food from your favorite restaurants.
-          </p>
-
-          <button className="order-btn">
-            Order Now
-          </button>
-
-        </div>
-
-        <div className="right">
-          <img
-            src="https://images.unsplash.com/photo-1504674900247-0877df9cc836"
-            alt="food"
-          />
-        </div>
-
-      </section>
-
-    </div>
-  )
+  return <Dashboard />
 }
 
 export default App
